5487046a397ccfaeba6c3a0e656ad5c10fa5a31c
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-11-04 21:56:02 +0530

    checkboxes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,69 +7,136 @@ import Image from "next/image";
 
 export default function Home() {
   const router = useRouter();
-  const [ytUrl, setYtUrl] = useState("");
+  const [formData, setFormData] = useState({
+    ytUrl: "",
+    inDepth: false,
+    bullets: false,
+  });
 
-const handleSubmit = async () => {
-  try {
-    if (!ytUrl.includes("youtube") && !ytUrl.includes("youtu.be")) {
-      toast.error("Invalid YouTube URL!");
-      return;
+  const handleSubmit = async () => {
+    try {
+      const { ytUrl } = formData;
+      if (!ytUrl.includes("youtube") && !ytUrl.includes("youtu.be")) {
+        toast.error("Invalid YouTube URL!");
+        return;
+      }
+      let videoId = "";
+      if (ytUrl.includes("youtu.be")) {
+        videoId = ytUrl.split("youtu.be/")[1].split("?")[0];
+      } else {
+        videoId = ytUrl.split("v=")[1];
+      }
+      router.push(
+        `/summarize?videoId=${videoId}&inDepth=${formData.inDepth}&bullets=${formData.bullets}`
+      );
+    } catch (error: any) {
+      toast.error("Something Went Wrong!");
     }
-    let videoId = "";
-    if (ytUrl.includes("youtu.be")) {
-      videoId = ytUrl.split("youtu.be/")[1].split("?")[0];
-    } else {
-      videoId = ytUrl.split("v=")[1];
-    }
-    router.push(`/summarize?videoId=${videoId}`);
-  } catch (error: any) {
-    toast.error("Something Went Wrong!");
-  }
-};
+  };
 
   return (
-    <main
-      className="min-h-screen flex flex-col items-center lg:px-10"
-      style={{
-        backgroundImage: `url('bg.svg')`,
-        backgroundSize: "contain",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundColor: "white",
-      }}
-    >
+    <>
       <Toaster />
-      <header className="flex flex-col justify-center items-center pt-10">
-        <Image src="logo.svg" width={50} height={50} alt="logo"></Image>
-        <h1 className="font-bold text-2xl">VideoBriefs</h1>
-      </header>
-      <div className="text-center flex flex-col flex-1 pt-32 items-center">
-        <h1 className="font-bold text-5xl lg:text-7xl lg:px-10">
-          Don't Want to Watch the Whole Video? - Try VideoBriefs!
-        </h1>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit();
-          }}
-          className="flex flex-col items-center w-full sm:w-3/4 md:w-1/2 lg:w-2/5"
-        >
-          <input
-            type="text"
-            className="border bg-black rounded-3xl text-white text-xl p-2 mt-14 text-center lg:w-full"
-            placeholder="YouTube URL"
-            name="url"
-            value={ytUrl}
-            onChange={(e) => setYtUrl(e.target.value)}
-          ></input>
-          <button
-            type="submit"
-            className="border bg-black rounded-2xl text-white p-1.5 mt-4"
+      <main
+        className="min-h-screen flex flex-col items-center lg:px-10"
+        style={{
+          backgroundImage: `url('bg.svg')`,
+          backgroundSize: "contain",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: "white",
+        }}
+      >
+        <header className="flex flex-col justify-center items-center pt-10">
+          <Image src="logo.svg" width={50} height={50} alt="logo"></Image>
+          <h1 className="font-bold text-2xl">VideoBriefs</h1>
+        </header>
+        <div className="text-center flex flex-col flex-1 pt-32 items-center">
+          <h1 className="font-bold text-5xl lg:text-7xl lg:px-10">
+            Don't want to watch the entire video? – Try VideoBriefs!
+          </h1>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+            className="flex flex-col items-center w-full sm:w-3/4 md:w-1/2 lg:w-2/5"
           >
-            Summarize
-          </button>
-        </form>
-      </div>
-    </main>
+            <input
+              type="text"
+              className="border bg-black rounded-3xl text-white text-xl p-2 mt-14 text-center lg:w-full"
+              placeholder="YouTube URL"
+              name="url"
+              value={formData.ytUrl}
+              onChange={(e) =>
+                setFormData((prev) => ({ ...prev, ytUrl: e.target.value }))
+              }
+            ></input>
+            <div className="my-1">
+              <style jsx>{`
+                .checkbox {
+                  appearance: none;
+                  -webkit-appearance: none;
+                  width: 20px;
+                  height: 20px;
+                  background: white;
+                  border: 2px solid black;
+                  border-radius: 4px;
+                  position: relative;
+                  vertical-align: middle;
+                }
+                .checkbox:checked {
+                  background: black;
+                }
+                .checkbox:checked::after {
+                  content: "✔";
+                  color: white;
+                  position: absolute;
+                  top: 50%;
+                  left: 50%;
+                  transform: translate(-50%, -50%);
+                }
+              `}</style>
+              <label className="mx-2">
+                <input
+                  className="checkbox"
+                  type="checkbox"
+                  name="indepth"
+                  checked={formData.inDepth}
+                  onChange={(e) =>
+                    setFormData((prev) => ({
+                      ...prev,
+                      inDepth: e.target.checked,
+                    }))
+                  }
+                ></input>{" "}
+                In-Depth
+              </label>
+              <label className="mx-2">
+                <input
+                  className="checkbox"
+                  type="checkbox"
+                  name="bullet"
+                  checked={formData.bullets}
+                  onChange={(e) =>
+                    setFormData((prev) => ({
+                      ...prev,
+                      bullets: e.target.checked,
+                    }))
+                  }
+                ></input>{" "}
+                Bullet Points
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="border bg-black rounded-2xl text-white p-1.5 mt-4"
+            >
+              Summarize
+            </button>
+          </form>
+        </div>
+      </main>
+    </>
   );
 }
--- src/app/summarize/page.tsx
+++ src/app/summarize/page.tsx
@@ -14,39 +14,45 @@ export default function SummarizePage() {
     toast.success("Summarizing...");
     const urlParams = new URLSearchParams(window.location.search);
     const videoId = urlParams.get("videoId");
+    const inDepth = urlParams.get("inDepth");
+    const bullets = urlParams.get("bullets");
     const getData = async () => {
-      const res = await axios.post("/api/summarize", { videoId });
+      const res = await axios.post("/api/summarize", {
+        videoId,
+        inDepth,
+        bullets,
+      });
       setData(res.data);
       setLoading(false);
     };
     getData();
   }, []);
   return (
-    <main
-      className="min-h-screen"
-      style={{
-        backgroundColor: "white",
-      }}
-    >
+    <>
       <Toaster />
-      <header className="flex flex-col justify-center items-center pt-10">
-        <Image src="logo.svg" width={50} height={50} alt="logo"></Image>
-        <h1 className="font-bold text-2xl">VideoBriefs</h1>
-      </header>
-      <div className="text-center flex flex-col pt-16 items-center lg:px-32">
-        {loading ? (
-          <div className="flex justify-center items-center h-96">
-            <ClipLoader className="flex-1" />
-          </div>
-        ) : (
-          <div>
-            <h1 className="text-4xl font-bold pb-10">
-              {data.title}
-            </h1>
-            <h1>{data.summary}</h1>
-          </div>
-        )}
-      </div>
-    </main>
+      <main
+        className="min-h-screen"
+        style={{
+          backgroundColor: "white",
+        }}
+      >
+        <header className="flex flex-col justify-center items-center pt-10">
+          <Image src="logo.svg" width={50} height={50} alt="logo"></Image>
+          <h1 className="font-bold text-2xl">VideoBriefs</h1>
+        </header>
+        <div className="text-center flex flex-col pt-16 items-center lg:px-32">
+          {loading ? (
+            <div className="flex justify-center items-center h-96">
+              <ClipLoader className="flex-1" />
+            </div>
+          ) : (
+            <div>
+              <h1 className="text-4xl font-bold pb-10">{data.title}</h1>
+              <h1>{data.summary}</h1>
+            </div>
+          )}
+        </div>
+      </main>
+    </>
   );
 }
