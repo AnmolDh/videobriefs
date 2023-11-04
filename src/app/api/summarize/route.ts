3b24f15f0b9644be3a031c60527db3371179da41
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-11-05 01:22:52 +0530

    shift to edge fun

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -1,6 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Client } from "youtubei";
 import OpenAI from "openai";
+import { OpenAIStream } from "@/helpers/OpenAIStream";
+
+export const config = {
+  runtime: "edge",
+};
 
 export async function POST(req: NextRequest) {
   try {
@@ -17,35 +22,82 @@ export async function POST(req: NextRequest) {
       ts += item.text + " ";
     });
 
-    const completion = await openai.chat.completions.create({
-      messages: [
-        {
-          role: "user",
-          content: `${ts} summarize this youtube video transcript ${
-            inDepth ? "in deep depth" : ""
-          }. Respond with JSON Object with title and summary (array of points) key.`,
-        },
-      ],
-      model: "gpt-3.5-turbo",
-    });
+    const prompt = `${ts} summarize this youtube video transcript ${
+      inDepth ? "in deep depth" : ""
+    }.`;
 
-    const messageContent = completion.choices[0].message.content;
-    let res = null;
-    if (messageContent) {
-      try {
-        console.log(messageContent);
-        res = JSON.parse(messageContent);
-        console.log(res);
-      } catch (error: any) {
-        return NextResponse.json(
-          { message: "Invalid response from OpenAI" },
-          { status: 500 }
-        );
-      }
-    }
-
-    return NextResponse.json(res, { status: 200 });
+    const payload = {
+      model: "text-davinci-003",
+      prompt,
+      temperature: 0.7,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      max_tokens: 200,
+      stream: true,
+      n: 1,
+    };
+    const stream = await OpenAIStream(payload);
+    console.log(stream);
+    return new Response(stream);
   } catch (error: any) {
     return NextResponse.json({ message: error.message }, { status: 500 });
   }
 }
+
+
+
+
+
+
+/* SERVERLESS FUNCTION */
+
+// import { NextRequest, NextResponse } from "next/server";
+// import { Client } from "youtubei";
+// import OpenAI from "openai";
+
+// export async function POST(req: NextRequest) {
+//   try {
+//     const reqBody = await req.json();
+//     const { videoId, inDepth, bullets } = reqBody;
+
+//     const youtube = new Client();
+//     const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY || "" });
+
+//     const tsData = await youtube.getVideoTranscript(videoId);
+
+//     let ts = "";
+//     tsData?.forEach((item) => {
+//       ts += item.text + " ";
+//     });
+
+//     const completion = await openai.chat.completions.create({
+//       messages: [
+//         {
+//           role: "user",
+//           content: `${ts} summarize this youtube video transcript ${
+//             inDepth ? "in deep depth" : ""
+//           }. Respond with JSON Object with title and summary (array of points) key.`,
+//         },
+//       ],
+//       model: "gpt-3.5-turbo",
+//     });
+
+//     const messageContent = completion.choices[0].message.content;
+//     let res = null;
+//     if (messageContent) {
+//       try {
+//         res = JSON.parse(messageContent);
+//       } catch (error: any) {
+//         return NextResponse.json(
+//           { message: "Invalid response from OpenAI" },
+//           { status: 500 }
+//         );
+//       }
+//     }
+
+//     return NextResponse.json(res, { status: 200 });
+//   } catch (error: any) {
+//     return NextResponse.json({ message: error.message }, { status: 500 });
+//   }
+// }
--- src/app/summarize/page.tsx
+++ src/app/summarize/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import axios from "axios";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
@@ -8,10 +7,7 @@ import { ClipLoader } from "react-spinners";
 
 export default function SummarizePage() {
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState({
-    title: "",
-    summary: []
-  });
+  const [summary, setSummary] = useState("");
 
   useEffect(() => {
     toast.success("Summarizing...");
@@ -20,13 +16,34 @@ export default function SummarizePage() {
     const inDepth = urlParams.get("inDepth");
     const bullets = urlParams.get("bullets");
     const getData = async () => {
-      const res = await axios.post("/api/summarize", {
-        videoId,
-        inDepth,
-        bullets,
+      const res = await fetch("/api/summarize", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          videoId,
+          inDepth,
+          bullets,
+        }),
       });
-      setData(res.data);
+
+      if (!res.body) {
+        throw new Error("Response body is null");
+      }
       setLoading(false);
+      const reader = res.body.getReader();
+      const decoder = new TextDecoder();
+      let done = false;
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        if (value) {
+          const chunkValue = decoder.decode(value);
+          setSummary((prev) => prev + chunkValue);
+        }
+      }
     };
     getData();
   }, []);
@@ -50,18 +67,9 @@ export default function SummarizePage() {
             </div>
           ) : (
             <div>
-              <h1 className="text-4xl font-bold pb-10 text-left">
-                {data.title || ""}
+              <h1 className="pb-10 text-left">
+                {summary ? summary : "No Summary Found"}
               </h1>
-              <ul>
-                {data.summary.map((e, i) => {
-                  return (
-                    <li key={i} className="list-disc text-left pb-2 text-lg font-medium">
-                      {e}
-                    </li>
-                  );
-                })}
-              </ul>
             </div>
           )}
         </div>
@@ -69,3 +77,85 @@ export default function SummarizePage() {
     </>
   );
 }
+
+
+
+
+
+
+/* SERVERLESS FUNCTION */
+
+// "use client";
+
+// import axios from "axios";
+// import Image from "next/image";
+// import { useEffect, useState } from "react";
+// import toast, { Toaster } from "react-hot-toast";
+// import { ClipLoader } from "react-spinners";
+
+// export default function SummarizePage() {
+//   const [loading, setLoading] = useState(true);
+//   const [data, setData] = useState({
+//     title: "",
+//     summary: [],
+//   });
+
+//   useEffect(() => {
+//     toast.success("Summarizing...");
+//     const urlParams = new URLSearchParams(window.location.search);
+//     const videoId = urlParams.get("videoId");
+//     const inDepth = urlParams.get("inDepth");
+//     const bullets = urlParams.get("bullets");
+//     const getData = async () => {
+//       const res = await axios.post("/api/summarize", {
+//         videoId,
+//         inDepth,
+//         bullets,
+//       });
+//       setData(res.data);
+//       setLoading(false);
+//     };
+//     getData();
+//   }, []);
+//   return (
+//     <>
+//       <Toaster />
+//       <main
+//         className="min-h-screen"
+//         style={{
+//           backgroundColor: "white",
+//         }}
+//       >
+//         <header className="flex flex-col justify-center items-center pt-10">
+//           <Image src="logo.svg" width={50} height={50} alt="logo"></Image>
+//           <h1 className="font-bold text-2xl">VideoBriefs</h1>
+//         </header>
+//         <div className="text-center flex flex-col pt-16 items-center px-6 lg:px-32">
+//           {loading ? (
+//             <div className="flex justify-center items-center h-96">
+//               <ClipLoader className="flex-1" />
+//             </div>
+//           ) : (
+//             <div>
+//               <h1 className="text-4xl font-bold pb-10 text-left">
+//                 {data.title || ""}
+//               </h1>
+//               <ul>
+//                 {data.summary.map((e, i) => {
+//                   return (
+//                     <li
+//                       key={i}
+//                       className="list-disc text-left pb-2 text-lg font-medium"
+//                     >
+//                       {e}
+//                     </li>
+//                   );
+//                 })}
+//               </ul>
+//             </div>
+//           )}
+//         </div>
+//       </main>
+//     </>
+//   );
+// }
